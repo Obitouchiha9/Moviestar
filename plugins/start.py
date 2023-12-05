322a73a9e47433a94ea05a6b55982f9103b4d774
Obitouchiha9 <149180408+Obitouchiha9@users.noreply.github.com>
2023-12-05 19:23:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("👿 About Me", callback_data = "about"),
+                    InlineKeyboardButton("💥 Close", callback_data = "close")
                 ]
             ]
         )
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "Join Channel🥺",
                 url = client.invitelink)
         ]
     ]
@@ -131,7 +131,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = 'Reload⚡',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
@@ -189,7 +189,7 @@ async def send_text(client: Bot, message: Message):
                 pass
             total += 1
         
-        status = f"""<b><u>Broadcast Completed</u>
+        status = f"""<b><u>Broadcast Ho Gya Hai</u>
 
 Total Users: <code>{total}</code>
 Successful: <code>{successful}</code>
